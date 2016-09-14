3023cbfd70a839b9c1e87e061969305165a64b1e
Emanuel Borsboom <manny@fpcomplete.com>
2016-09-13 19:31:56 -0700

    upload: stop setting Github release file label

--- upload-installers.sh
+++ upload-installers.sh
@@ -8,12 +8,12 @@ UPLOAD_URL=$(curl -sSLH "Authorization: token $GITHUB_AUTH_TOKEN" https://api.gi
 
 gpg --digest-algo=sha512 --detach-sig --armor -u 0x575159689BEFB442 stack-$STACKVER-windows-i386-installer.exe
 
-curl -X POST --data-binary "@stack-$STACKVER-windows-i386-installer.exe" -H "Content-type: application/octet-stream" -H "Authorization: token $GITHUB_AUTH_TOKEN" "$UPLOAD_URL?name=stack-$STACKVER-windows-i386-installer.exe&label=Windows+32-bit+Installer"
+curl -X POST --data-binary "@stack-$STACKVER-windows-i386-installer.exe" -H "Content-type: application/octet-stream" -H "Authorization: token $GITHUB_AUTH_TOKEN" "$UPLOAD_URL?name=stack-$STACKVER-windows-i386-installer.exe"
 
-curl -X POST --data-binary "@stack-$STACKVER-windows-i386-installer.exe.asc" -H "Content-type: application/octet-stream" -H "Authorization: token $GITHUB_AUTH_TOKEN" "$UPLOAD_URL?name=stack-$STACKVER-windows-i386-installer.exe.asc&label=Windows+32-bit+Installer+(GPG+Signature)"
+curl -X POST --data-binary "@stack-$STACKVER-windows-i386-installer.exe.asc" -H "Content-type: application/octet-stream" -H "Authorization: token $GITHUB_AUTH_TOKEN" "$UPLOAD_URL?name=stack-$STACKVER-windows-i386-installer.exe.asc"
 
 gpg --detach-sig --armor -u 9BEFB442 stack-$STACKVER-windows-x86_64-installer.exe
 
-curl -X POST --data-binary "@stack-$STACKVER-windows-x86_64-installer.exe" -H "Content-type: application/octet-stream" -H "Authorization: token $GITHUB_AUTH_TOKEN" "$UPLOAD_URL?name=stack-$STACKVER-windows-x86_64-installer.exe&label=Windows+64-bit+Installer"
+curl -X POST --data-binary "@stack-$STACKVER-windows-x86_64-installer.exe" -H "Content-type: application/octet-stream" -H "Authorization: token $GITHUB_AUTH_TOKEN" "$UPLOAD_URL?name=stack-$STACKVER-windows-x86_64-installer.exe"
 
-curl -X POST --data-binary "@stack-$STACKVER-windows-x86_64-installer.exe.asc" -H "Content-type: application/octet-stream" -H "Authorization: token $GITHUB_AUTH_TOKEN" "$UPLOAD_URL?name=stack-$STACKVER-windows-x86_64-installer.exe.asc&label=Windows+64-bit+Installer+(GPG+Signature)"
+curl -X POST --data-binary "@stack-$STACKVER-windows-x86_64-installer.exe.asc" -H "Content-type: application/octet-stream" -H "Authorization: token $GITHUB_AUTH_TOKEN" "$UPLOAD_URL?name=stack-$STACKVER-windows-x86_64-installer.exe.asc"
